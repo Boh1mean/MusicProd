5b0c80a7cba425969daecc8db2658c0a2e76f177
Vgssy <danya.knyazkin@gmail.com>
2026-04-30 01:57:54 +0300

    fix migr

--- db/migrate/20241209165337_fix_tracks_table.rb
+++ db/migrate/20241209165337_fix_tracks_table.rb
@@ -1,14 +1,17 @@
 class FixTracksTable < ActiveRecord::Migration[8.0]
   def change
     change_table :tracks do |t|
-      t.string :artist_name, null: false
-      t.string :release_date, null: false
-      t.string :kind, null: false
-      t.string :artist_id, null: false
-      t.string :artist_url, null: false
-      t.string :content_advisory_rating
-      t.string :artwork_url, null: false
-      t.text :genres, null: false, array: true, default: []
+      t.string :artist_name unless column_exists?(:tracks, :artist_name)
+      t.string :release_date unless column_exists?(:tracks, :release_date)
+      t.string :kind unless column_exists?(:tracks, :kind)
+      t.string :artist_id unless column_exists?(:tracks, :artist_id)
+      t.string :artist_url unless column_exists?(:tracks, :artist_url)
+      t.string :content_advisory_rating unless column_exists?(:tracks, :content_advisory_rating)
+      t.string :artwork_url unless column_exists?(:tracks, :artwork_url)
+
+      unless column_exists?(:tracks, :genres)
+        t.text :genres, array: true, default: []
+      end
     end
   end
 end
